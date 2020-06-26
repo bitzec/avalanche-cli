gin/ava-cli

CLI: platform/*.sh

cmd/platform/add-default-subnet-delegator.test.sh
@@ -1,0 +1,77 @@
+#!/usr/bin/env bash
+###############################################################################
+
+function cmd {
+    printf "./ava-cli.sh platform add-default-subnet-delegator" ;
+}
+
+function check {
+    local result="$1" ;
+    local result_u ; result_u=$(printf '%s' "$result" | cut -d' ' -f3) ;
+    local result_h ; result_h=$(printf '%s' "$result" | cut -d' ' -f5) ;
+    local result_d ; result_d=$(printf '%s' "$result" | cut -d' ' -f7) ;
+    local expect_u ; expect_u="'127.0.0.1:9650/ext/P'" ;
+    assertEquals "$expect_u" "$result_u" ;
+    local expect_h ; expect_h="'content-type:application/json'" ;
+    assertEquals "$expect_h" "$result_h" ;
+    local expect_d ; expect_d="'{" ;
+    expect_d+='"jsonrpc":"2.0",' ;
+    expect_d+='"id":1,' ;
+    expect_d+='"method":"platform.addDefaultSubnetDelegator",' ;
+    expect_d+='"params":{' ;
+    expect_d+='"id":"ID",' ;
+    expect_d+='"startTime":2000000000,' ;
+    expect_d+='"endTime":3000000000,' ;
+    expect_d+='"stakeAmount":1000000,' ;
+    expect_d+='"payerNonce":3,' ;
+    expect_d+='"destination":"DESTINATION"' ;
+    expect_d+="}}'" ;
+    assertEquals "$expect_d" "$result_d" ;
+    local expect="curl --url $expect_u --header $expect_h --data $expect_d" ;
+    assertEquals "$expect" "$result" ;
+}
+
+function test_platform__add_default_subnet_delegator_1a {
+    check "$(RPC_ID=1 $(cmd) \
+        -i ID -b 2000000000 -e 3000000000 \
+        -# 1000000 -% 3 -d DESTINATION)" ;
+}
+
+function test_platform__add_default_subnet_delegator_1b {
+    check "$(RPC_ID=1 AVA_ID=ID $(cmd) \
+        -b 2000000000 -e 3000000000 \
+        -# 1000000 -% 3 -d DESTINATION)" ;
+}
+
+function test_platform__add_default_subnet_delegator_1c {
+    check "$(RPC_ID=1 AVA_START_TIME=2000000000 $(cmd) \
+        -i ID -e 3000000000 \
+        -# 1000000 -% 3 -d DESTINATION)" ;
+}
+
+function test_platform__add_default_subnet_delegator_1d {
+    check "$(RPC_ID=1 AVA_END_TIME=3000000000 $(cmd) \
+        -i ID -b 2000000000 \
+        -# 1000000 -% 3 -d DESTINATION)" ;
+}
+
+function test_platform__add_default_subnet_delegator_1e {
+    check "$(RPC_ID=1 AVA_STAKE_AMOUNT=1000000 $(cmd) \
+        -i ID -b 2000000000 -e 3000000000 \
+        -% 3 -d DESTINATION)" ;
+}
+
+function test_platform__add_default_subnet_delegator_1f {
+    check "$(RPC_ID=1 AVA_PAYER_NONCE=3 $(cmd) \
+        -i ID -b 2000000000 -e 3000000000 \
+        -# 1000000 -d DESTINATION)" ;
+}
+
+function test_platform__add_default_subnet_delegator_1g {
+    check "$(RPC_ID=1 AVA_DESTINATION=DESTINATION $(cmd) \
+        -i ID -b 2000000000 -e 3000000000 \
+        -# 1000000 -% 3)" ;
+}
+
+###############################################################################
+###############################################################################

cmd/platform/get-account.test.sh
@@ -1,0 +1,39 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC2091
+###############################################################################
+
+function cmd {
+    printf "./ava-cli.sh platform get-account" ;
+}
+
+function check {
+    local result="$1" ;
+    local result_u ; result_u=$(printf '%s' "$result" | cut -d' ' -f3) ;
+    local result_h ; result_h=$(printf '%s' "$result" | cut -d' ' -f5) ;
+    local result_d ; result_d=$(printf '%s' "$result" | cut -d' ' -f7) ;
+    local expect_u ; expect_u="'127.0.0.1:9650/ext/P'" ;
+    assertEquals "$expect_u" "$result_u" ;
+    local expect_h ; expect_h="'content-type:application/json'" ;
+    assertEquals "$expect_h" "$result_h" ;
+    local expect_d ; expect_d="'{" ;
+    expect_d+='"jsonrpc":"2.0",' ;
+    expect_d+='"id":1,' ;
+    expect_d+='"method":"platform.getAccount",' ;
+    expect_d+='"params":{' ;
+    expect_d+='"address":"ADDRESS"' ;
+    expect_d+="}}'" ;
+    assertEquals "$expect_d" "$result_d" ;
+    local expect="curl --url $expect_u --header $expect_h --data $expect_d" ;
+    assertEquals "$expect" "$result" ;
+}
+
+function test_platform__get_account_1a {
+    check "$(RPC_ID=1 $(cmd) -@ ADDRESS)" ;
+}
+
+function test_platform__get_account_1b {
+    check "$(RPC_ID=1 AVA_ADDRESS=ADDRESS $(cmd))" ;
+}
+
+###############################################################################
+###############################################################################

cmd/platform/get-blockchains.sh
@@ -1,0 +1,81 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC1090,SC2214
+###############################################################################
+PLATFORM_SCRIPT=$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)
+###############################################################################
+
+source "$PLATFORM_SCRIPT/../../cli/color.sh" ;
+source "$PLATFORM_SCRIPT/../../cli/command.sh" ;
+source "$PLATFORM_SCRIPT/../../cli/rpc/data.sh" ;
+source "$PLATFORM_SCRIPT/../../cli/rpc/post.sh" ;
+
+###############################################################################
+###############################################################################
+
+function cli_help {
+    local usage ;
+    usage="${BB}Usage:${NB} $(command_fqn "${0}")" ;
+    usage+=" [-N|--node=\${AVA_NODE-127.0.0.1:9650}]" ;
+    usage+=" [-S|--silent-rpc|\${AVA_SILENT_RPC}]" ;
+    usage+=" [-V|--verbose-rpc|\${AVA_VERBOSE_RPC}]" ;
+    usage+=" [-Y|--yes-run-rpc|\${AVA_YES_RUN_RPC}]" ;
+    usage+=" [-h|--help]" ;
+    source "$PLATFORM_SCRIPT/../../cli/help.sh" ; # shellcheck disable=2046
+    printf '%s\n\n%s\n' "$usage" "$(help_for $(command_fqn "${0}"))" ;
+}
+
+function cli_options {
+    local -a options ;
+    options+=( "-N" "--node=" ) ;
+    options+=( "-S" "--silent-rpc" ) ;
+    options+=( "-V" "--verbose-rpc" ) ;
+    options+=( "-Y" "--yes-run-rpc" ) ;
+    options+=( "-h" "--help" ) ;
+    printf '%s ' "${options[@]}" ;
+}
+
+function cli {
+    while getopts ":hSVYN:-:" OPT "$@"
+    do
+        if [ "$OPT" = "-" ] ; then
+            OPT="${OPTARG%%=*}" ;
+            OPTARG="${OPTARG#$OPT}" ;
+            OPTARG="${OPTARG#=}" ;
+        fi
+        case "${OPT}" in
+            list-options)
+                cli_options && exit 0 ;;
+            N|node)
+                AVA_NODE="${OPTARG}" ;;
+            S|silent-rpc)
+                export AVA_SILENT_RPC=1 ;;
+            V|verbose-rpc)
+                export AVA_VERBOSE_RPC=1 ;;
+            Y|yes-run-rpc)
+                export AVA_YES_RUN_RPC=1 ;;
+            h|help)
+                cli_help && exit 0 ;;
+            :|*)
+                cli_help && exit 1 ;;
+        esac
+    done
+    if [ -z "$AVA_NODE" ] ; then
+        AVA_NODE="127.0.0.1:9650" ;
+    fi
+    shift $((OPTIND-1)) ;
+}
+
+function rpc_method {
+    printf "platform.getBlockchains" ;
+}
+
+function rpc_params {
+    printf "{}" ;
+}
+
+###############################################################################
+
+cli "$@" && rpc_post "$AVA_NODE/ext/P" "$(rpc_data)" ;
+
+###############################################################################
+###############################################################################

cmd/platform/get-pending-validators.test.sh
@@ -1,0 +1,43 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC2091
+###############################################################################
+
+function cmd {
+    printf "./ava-cli.sh platform get-pending-validators" ;
+}
+
+function check {
+    local result="$1" ;
+    local result_u ; result_u=$(printf '%s' "$result" | cut -d' ' -f3) ;
+    local result_h ; result_h=$(printf '%s' "$result" | cut -d' ' -f5) ;
+    local result_d ; result_d=$(printf '%s' "$result" | cut -d' ' -f7) ;
+    local expect_u ; expect_u="'127.0.0.1:9650/ext/P'" ;
+    assertEquals "$expect_u" "$result_u" ;
+    local expect_h ; expect_h="'content-type:application/json'" ;
+    assertEquals "$expect_h" "$result_h" ;
+    local expect_d ; expect_d="'{" ;
+    expect_d+='"jsonrpc":"2.0",' ;
+    expect_d+='"id":1,' ;
+    expect_d+='"method":"platform.getPendingValidators",' ;
+    expect_d+='"params":{' ;
+    expect_d+='"subnetID":'"$([ -n "$2" ] && echo "\"$2\"" || echo null)" ;
+    expect_d+="}}'" ;
+    assertEquals "$expect_d" "$result_d" ;
+    local expect="curl --url $expect_u --header $expect_h --data $expect_d" ;
+    assertEquals "$expect" "$result" ;
+}
+
+function test_platform__get_pending_validators_1a {
+    check "$(RPC_ID=1 $(cmd) -s SUBNET_ID)" SUBNET_ID ;
+}
+
+function test_platform__get_pending_validators_1b {
+    check "$(RPC_ID=1 AVA_SUBNET_ID=SUBNET_ID $(cmd))" SUBNET_ID ;
+}
+
+function test_platform__get_pending_validators_1c {
+    check "$(RPC_ID=1 $(cmd))" ;
+}
+
+###############################################################################
+###############################################################################

cmd/platform/list-accounts.test.sh
@@ -1,0 +1,43 @@
+#!/usr/bin/env bash
+###############################################################################
+
+function cmd {
+    printf "./ava-cli.sh platform list-accounts" ;
+}
+
+function check {
+    local result="$1" ;
+    local result_u ; result_u=$(printf '%s' "$result" | cut -d' ' -f3) ;
+    local result_h ; result_h=$(printf '%s' "$result" | cut -d' ' -f5) ;
+    local result_d ; result_d=$(printf '%s' "$result" | cut -d' ' -f7) ;
+    local expect_u ; expect_u="'127.0.0.1:9650/ext/P'" ;
+    assertEquals "$expect_u" "$result_u" ;
+    local expect_h ; expect_h="'content-type:application/json'" ;
+    assertEquals "$expect_h" "$result_h" ;
+    local expect_d ; expect_d="'{" ;
+    expect_d+='"jsonrpc":"2.0",' ;
+    expect_d+='"id":1,' ;
+    expect_d+='"method":"platform.listAccounts",' ;
+    expect_d+='"params":{' ;
+    expect_d+='"username":"USERNAME",' ;
+    expect_d+='"password":"PASSWORD"' ;
+    expect_d+="}}'" ;
+    assertEquals "$expect_d" "$result_d" ;
+    local expect="curl --url $expect_u --header $expect_h --data $expect_d" ;
+    assertEquals "$expect" "$result" ;
+}
+
+function test_platform__list_accounts_1 {
+    check "$(RPC_ID=1 $(cmd) -u USERNAME -p PASSWORD)" ;
+}
+
+function test_platform__list_accounts_1d {
+    check "$(RPC_ID=1 AVA_USERNAME=USERNAME $(cmd) -p PASSWORD)" ;
+}
+
+function test_platform__list_accounts_1e {
+    check "$(RPC_ID=1 AVA_PASSWORD=PASSWORD $(cmd) -u USERNAME)" ;
+}
+
+###############################################################################
+###############################################################################

cmd/platform/sample-validators.test.sh
@@ -1,0 +1,48 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC2091
+###############################################################################
+
+function cmd {
+    printf "./ava-cli.sh platform sample-validators" ;
+}
+
+function check {
+    local result="$1" ;
+    local result_u ; result_u=$(printf '%s' "$result" | cut -d' ' -f3) ;
+    local result_h ; result_h=$(printf '%s' "$result" | cut -d' ' -f5) ;
+    local result_d ; result_d=$(printf '%s' "$result" | cut -d' ' -f7) ;
+    local expect_u ; expect_u="'127.0.0.1:9650/ext/P'" ;
+    assertEquals "$expect_u" "$result_u" ;
+    local expect_h ; expect_h="'content-type:application/json'" ;
+    assertEquals "$expect_h" "$result_h" ;
+    local expect_d ; expect_d="'{" ;
+    expect_d+='"jsonrpc":"2.0",' ;
+    expect_d+='"id":1,' ;
+    expect_d+='"method":"platform.sampleValidators",' ;
+    expect_d+='"params":{' ;
+    expect_d+='"size":3,' ;
+    expect_d+='"subnetID":'"$([ -n "$2" ] && echo "\"$2\"" || echo null)" ;
+    expect_d+="}}'" ;
+    assertEquals "$expect_d" "$result_d" ;
+    local expect="curl --url $expect_u --header $expect_h --data $expect_d" ;
+    assertEquals "$expect" "$result" ;
+}
+
+function test_platform__sample_validators_1a {
+    check "$(RPC_ID=1 $(cmd) -n 3 -s SUBNET_ID)" SUBNET_ID ;
+}
+
+function test_platform__sample_validators_1b {
+    check "$(RPC_ID=1 AVA_SIZE=3 $(cmd) -s SUBNET_ID)" SUBNET_ID ;
+}
+
+function test_platform__sample_validators_1c {
+    check "$(RPC_ID=1 AVA_SUBNET_ID=SUBNET_ID $(cmd) -n 3)" SUBNET_ID ;
+}
+
+function test_platform__sample_validators_1d {
+    check "$(RPC_ID=1 $(cmd) -n 3)" ;
+}
+
+###############################################################################
+###############################################################################

bitzec/avalanche-cli

cmd/platform/add-default-subnet-delegator.test.sh
@@ -1,0 +1,77 @@
+#!/usr/bin/env bash
+###############################################################################
+
+function cmd {
+    printf "./avalanche-cli.sh platform add-default-subnet-delegator" ;
+}
+
+function check {
+    local result="$1" ;
+    local result_u ; result_u=$(printf '%s' "$result" | cut -d' ' -f3) ;
+    local result_h ; result_h=$(printf '%s' "$result" | cut -d' ' -f5) ;
+    local result_d ; result_d=$(printf '%s' "$result" | cut -d' ' -f7) ;
+    local expect_u ; expect_u="'127.0.0.1:9650/ext/P'" ;
+    assertEquals "$expect_u" "$result_u" ;
+    local expect_h ; expect_h="'content-type:application/json'" ;
+    assertEquals "$expect_h" "$result_h" ;
+    local expect_d ; expect_d="'{" ;
+    expect_d+='"jsonrpc":"2.0",' ;
+    expect_d+='"id":1,' ;
+    expect_d+='"method":"platform.addDefaultSubnetDelegator",' ;
+    expect_d+='"params":{' ;
+    expect_d+='"id":"ID",' ;
+    expect_d+='"startTime":2000000000,' ;
+    expect_d+='"endTime":3000000000,' ;
+    expect_d+='"stakeAmount":1000000,' ;
+    expect_d+='"payerNonce":3,' ;
+    expect_d+='"destination":"DESTINATION"' ;
+    expect_d+="}}'" ;
+    assertEquals "$expect_d" "$result_d" ;
+    local expect="curl --url $expect_u --header $expect_h --data $expect_d" ;
+    assertEquals "$expect" "$result" ;
+}
+
+function test_platform__add_default_subnet_delegator_1a {
+    check "$(RPC_ID=1 $(cmd) \
+        -i ID -b 2000000000 -e 3000000000 \
+        -# 1000000 -% 3 -d DESTINATION)" ;
+}
+
+function test_platform__add_default_subnet_delegator_1b {
+    check "$(RPC_ID=1 AVA_ID=ID $(cmd) \
+        -b 2000000000 -e 3000000000 \
+        -# 1000000 -% 3 -d DESTINATION)" ;
+}
+
+function test_platform__add_default_subnet_delegator_1c {
+    check "$(RPC_ID=1 AVA_START_TIME=2000000000 $(cmd) \
+        -i ID -e 3000000000 \
+        -# 1000000 -% 3 -d DESTINATION)" ;
+}
+
+function test_platform__add_default_subnet_delegator_1d {
+    check "$(RPC_ID=1 AVA_END_TIME=3000000000 $(cmd) \
+        -i ID -b 2000000000 \
+        -# 1000000 -% 3 -d DESTINATION)" ;
+}
+
+function test_platform__add_default_subnet_delegator_1e {
+    check "$(RPC_ID=1 AVA_STAKE_AMOUNT=1000000 $(cmd) \
+        -i ID -b 2000000000 -e 3000000000 \
+        -% 3 -d DESTINATION)" ;
+}
+
+function test_platform__add_default_subnet_delegator_1f {
+    check "$(RPC_ID=1 AVA_PAYER_NONCE=3 $(cmd) \
+        -i ID -b 2000000000 -e 3000000000 \
+        -# 1000000 -d DESTINATION)" ;
+}
+
+function test_platform__add_default_subnet_delegator_1g {
+    check "$(RPC_ID=1 AVA_DESTINATION=DESTINATION $(cmd) \
+        -i ID -b 2000000000 -e 3000000000 \
+        -# 1000000 -% 3)" ;
+}
+
+###############################################################################
+###############################################################################

cmd/platform/get-account.test.sh
@@ -1,0 +1,39 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC2091
+###############################################################################
+
+function cmd {
+    printf "./avalanche-cli.sh platform get-account" ;
+}
+
+function check {
+    local result="$1" ;
+    local result_u ; result_u=$(printf '%s' "$result" | cut -d' ' -f3) ;
+    local result_h ; result_h=$(printf '%s' "$result" | cut -d' ' -f5) ;
+    local result_d ; result_d=$(printf '%s' "$result" | cut -d' ' -f7) ;
+    local expect_u ; expect_u="'127.0.0.1:9650/ext/P'" ;
+    assertEquals "$expect_u" "$result_u" ;
+    local expect_h ; expect_h="'content-type:application/json'" ;
+    assertEquals "$expect_h" "$result_h" ;
+    local expect_d ; expect_d="'{" ;
+    expect_d+='"jsonrpc":"2.0",' ;
+    expect_d+='"id":1,' ;
+    expect_d+='"method":"platform.getAccount",' ;
+    expect_d+='"params":{' ;
+    expect_d+='"address":"ADDRESS"' ;
+    expect_d+="}}'" ;
+    assertEquals "$expect_d" "$result_d" ;
+    local expect="curl --url $expect_u --header $expect_h --data $expect_d" ;
+    assertEquals "$expect" "$result" ;
+}
+
+function test_platform__get_account_1a {
+    check "$(RPC_ID=1 $(cmd) -@ ADDRESS)" ;
+}
+
+function test_platform__get_account_1b {
+    check "$(RPC_ID=1 AVA_ADDRESS=ADDRESS $(cmd))" ;
+}
+
+###############################################################################
+###############################################################################

cmd/platform/get-blockchains.sh
@@ -1,0 +1,81 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC1090,SC2214
+###############################################################################
+CMD_SCRIPT=$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)
+###############################################################################
+
+source "$CMD_SCRIPT/../../cli/color.sh" ;
+source "$CMD_SCRIPT/../../cli/command.sh" ;
+source "$CMD_SCRIPT/../../cli/rpc/data.sh" ;
+source "$CMD_SCRIPT/../../cli/rpc/post.sh" ;
+
+###############################################################################
+###############################################################################
+
+function cli_help {
+    local usage ;
+    usage="${BB}Usage:${NB} $(command_fqn "${0}")" ;
+    usage+=" [-N|--node=\${AVA_NODE-127.0.0.1:9650}]" ;
+    usage+=" [-S|--silent-rpc|\${AVA_SILENT_RPC}]" ;
+    usage+=" [-V|--verbose-rpc|\${AVA_VERBOSE_RPC}]" ;
+    usage+=" [-Y|--yes-run-rpc|\${AVA_YES_RUN_RPC}]" ;
+    usage+=" [-h|--help]" ;
+    source "$CMD_SCRIPT/../../cli/help.sh" ; # shellcheck disable=2046
+    printf '%s\n\n%s\n' "$usage" "$(help_for $(command_fqn "${0}"))" ;
+}
+
+function cli_options {
+    local -a options ;
+    options+=( "-N" "--node=" ) ;
+    options+=( "-S" "--silent-rpc" ) ;
+    options+=( "-V" "--verbose-rpc" ) ;
+    options+=( "-Y" "--yes-run-rpc" ) ;
+    options+=( "-h" "--help" ) ;
+    printf '%s ' "${options[@]}" ;
+}
+
+function cli {
+    while getopts ":hSVYN:-:" OPT "$@"
+    do
+        if [ "$OPT" = "-" ] ; then
+            OPT="${OPTARG%%=*}" ;
+            OPTARG="${OPTARG#$OPT}" ;
+            OPTARG="${OPTARG#=}" ;
+        fi
+        case "${OPT}" in
+            list-options)
+                cli_options && exit 0 ;;
+            N|node)
+                AVA_NODE="${OPTARG}" ;;
+            S|silent-rpc)
+                export AVA_SILENT_RPC=1 ;;
+            V|verbose-rpc)
+                export AVA_VERBOSE_RPC=1 ;;
+            Y|yes-run-rpc)
+                export AVA_YES_RUN_RPC=1 ;;
+            h|help)
+                cli_help && exit 0 ;;
+            :|*)
+                cli_help && exit 1 ;;
+        esac
+    done
+    if [ -z "$AVA_NODE" ] ; then
+        AVA_NODE="127.0.0.1:9650" ;
+    fi
+    shift $((OPTIND-1)) ;
+}
+
+function rpc_method {
+    printf "platform.getBlockchains" ;
+}
+
+function rpc_params {
+    printf "{}" ;
+}
+
+###############################################################################
+
+cli "$@" && rpc_post "$AVA_NODE/ext/P" "$(rpc_data)" ;
+
+###############################################################################
+###############################################################################

cmd/platform/get-pending-validators.test.sh
@@ -1,0 +1,43 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC2091
+###############################################################################
+
+function cmd {
+    printf "./avalanche-cli.sh platform get-pending-validators" ;
+}
+
+function check {
+    local result="$1" ;
+    local result_u ; result_u=$(printf '%s' "$result" | cut -d' ' -f3) ;
+    local result_h ; result_h=$(printf '%s' "$result" | cut -d' ' -f5) ;
+    local result_d ; result_d=$(printf '%s' "$result" | cut -d' ' -f7) ;
+    local expect_u ; expect_u="'127.0.0.1:9650/ext/P'" ;
+    assertEquals "$expect_u" "$result_u" ;
+    local expect_h ; expect_h="'content-type:application/json'" ;
+    assertEquals "$expect_h" "$result_h" ;
+    local expect_d ; expect_d="'{" ;
+    expect_d+='"jsonrpc":"2.0",' ;
+    expect_d+='"id":1,' ;
+    expect_d+='"method":"platform.getPendingValidators",' ;
+    expect_d+='"params":{' ;
+    expect_d+='"subnetID":'"$([ -n "$2" ] && echo "\"$2\"" || echo null)" ;
+    expect_d+="}}'" ;
+    assertEquals "$expect_d" "$result_d" ;
+    local expect="curl --url $expect_u --header $expect_h --data $expect_d" ;
+    assertEquals "$expect" "$result" ;
+}
+
+function test_platform__get_pending_validators_1a {
+    check "$(RPC_ID=1 $(cmd) -s SUBNET_ID)" SUBNET_ID ;
+}
+
+function test_platform__get_pending_validators_1b {
+    check "$(RPC_ID=1 AVA_SUBNET_ID=SUBNET_ID $(cmd))" SUBNET_ID ;
+}
+
+function test_platform__get_pending_validators_1c {
+    check "$(RPC_ID=1 $(cmd))" ;
+}
+
+###############################################################################
+###############################################################################

cmd/platform/list-accounts.test.sh
@@ -1,0 +1,43 @@
+#!/usr/bin/env bash
+###############################################################################
+
+function cmd {
+    printf "./avalanche-cli.sh platform list-accounts" ;
+}
+
+function check {
+    local result="$1" ;
+    local result_u ; result_u=$(printf '%s' "$result" | cut -d' ' -f3) ;
+    local result_h ; result_h=$(printf '%s' "$result" | cut -d' ' -f5) ;
+    local result_d ; result_d=$(printf '%s' "$result" | cut -d' ' -f7) ;
+    local expect_u ; expect_u="'127.0.0.1:9650/ext/P'" ;
+    assertEquals "$expect_u" "$result_u" ;
+    local expect_h ; expect_h="'content-type:application/json'" ;
+    assertEquals "$expect_h" "$result_h" ;
+    local expect_d ; expect_d="'{" ;
+    expect_d+='"jsonrpc":"2.0",' ;
+    expect_d+='"id":1,' ;
+    expect_d+='"method":"platform.listAccounts",' ;
+    expect_d+='"params":{' ;
+    expect_d+='"username":"USERNAME",' ;
+    expect_d+='"password":"PASSWORD"' ;
+    expect_d+="}}'" ;
+    assertEquals "$expect_d" "$result_d" ;
+    local expect="curl --url $expect_u --header $expect_h --data $expect_d" ;
+    assertEquals "$expect" "$result" ;
+}
+
+function test_platform__list_accounts_1 {
+    check "$(RPC_ID=1 $(cmd) -u USERNAME -p PASSWORD)" ;
+}
+
+function test_platform__list_accounts_1d {
+    check "$(RPC_ID=1 AVA_USERNAME=USERNAME $(cmd) -p PASSWORD)" ;
+}
+
+function test_platform__list_accounts_1e {
+    check "$(RPC_ID=1 AVA_PASSWORD=PASSWORD $(cmd) -u USERNAME)" ;
+}
+
+###############################################################################
+###############################################################################

cmd/platform/sample-validators.test.sh
@@ -1,0 +1,48 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC2091
+###############################################################################
+
+function cmd {
+    printf "./avalanche-cli.sh platform sample-validators" ;
+}
+
+function check {
+    local result="$1" ;
+    local result_u ; result_u=$(printf '%s' "$result" | cut -d' ' -f3) ;
+    local result_h ; result_h=$(printf '%s' "$result" | cut -d' ' -f5) ;
+    local result_d ; result_d=$(printf '%s' "$result" | cut -d' ' -f7) ;
+    local expect_u ; expect_u="'127.0.0.1:9650/ext/P'" ;
+    assertEquals "$expect_u" "$result_u" ;
+    local expect_h ; expect_h="'content-type:application/json'" ;
+    assertEquals "$expect_h" "$result_h" ;
+    local expect_d ; expect_d="'{" ;
+    expect_d+='"jsonrpc":"2.0",' ;
+    expect_d+='"id":1,' ;
+    expect_d+='"method":"platform.sampleValidators",' ;
+    expect_d+='"params":{' ;
+    expect_d+='"size":3,' ;
+    expect_d+='"subnetID":'"$([ -n "$2" ] && echo "\"$2\"" || echo null)" ;
+    expect_d+="}}'" ;
+    assertEquals "$expect_d" "$result_d" ;
+    local expect="curl --url $expect_u --header $expect_h --data $expect_d" ;
+    assertEquals "$expect" "$result" ;
+}
+
+function test_platform__sample_validators_1a {
+    check "$(RPC_ID=1 $(cmd) -n 3 -s SUBNET_ID)" SUBNET_ID ;
+}
+
+function test_platform__sample_validators_1b {
+    check "$(RPC_ID=1 AVA_SIZE=3 $(cmd) -s SUBNET_ID)" SUBNET_ID ;
+}
+
+function test_platform__sample_validators_1c {
+    check "$(RPC_ID=1 AVA_SUBNET_ID=SUBNET_ID $(cmd) -n 3)" SUBNET_ID ;
+}
+
+function test_platform__sample_validators_1d {
+    check "$(RPC_ID=1 $(cmd) -n 3)" ;
+}
+
+###############################################################################
+###############################################################################